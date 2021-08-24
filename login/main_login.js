9bc64626a79f95e454ff245381c59abb26afcf53
henmarsl <hrlopezc@ul.edu.co>
2021-08-24 17:12:10 -0500

    mi primer commit

---
+++ login/main_login.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.login-container');
+ScrollReveal().reveal('.login-info-container', { delay: 500 });
+ScrollReveal().reveal('.social-login', { delay: 500 });
+ScrollReveal().reveal('.inputs-container', { delay: 500 });
+ScrollReveal().reveal('.image-container', { delay: 500 });
